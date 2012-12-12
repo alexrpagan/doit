af1bc2b663feccc01b1bc2ff5df380da403de138
Alex Pagan <alexrpagan@gmail.com>
2012-12-12 15:37:26 -0500

    add alt_root prefix to base urls

--- www/doitweb/urls.py
+++ www/doitweb/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -9,10 +10,10 @@ urlpatterns = patterns('',
     # (r'^doitweb/', include('doitweb.foo.urls')),
 
     # doit gui (first GUI)
-    (r'^doit/', include('doit.urls', namespace='doit')),
+    (r'^%sdoit/' % settings.ALT_ROOT, include('doit.urls', namespace='doit')),
 
     # data tamer console
-    (r'^tamer/', include('tamer.urls', namespace='tamer')),
+    (r'^%stamer/' % settings.ALT_ROOT, include('tamer.urls', namespace='tamer')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
