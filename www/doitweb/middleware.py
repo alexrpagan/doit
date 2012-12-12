dbad77e07163fb7c87494657da0db559cf93ad5e
Alex Pagan <alexrpagan@gmail.com>
2012-12-12 15:17:42 -0500

    add context processor for alternate root information

---
+++ www/doitweb/middleware.py
@@ -0,0 +1,11 @@
+from django.conf import settings
+
+# context processors...
+
+def url_context(request):
+    alt_root = ''
+    if settings.ALT_ROOT:
+        alt_root = ''.join(('/', settings.ALT_ROOT,))
+    return { 'url_context_on': True,
+             'base_url': settings.BASE_URL,
+             'alt_root': settings.ALT_ROOT }
